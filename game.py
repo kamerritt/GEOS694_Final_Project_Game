9f0f8ef464f569a04f413bee05e146aeba3c06bf
Karenna Merritt <kamerritt@alaska.edu>
2026-03-30 13:28:23 -0800

    initial gameplay code from tech with tim

---
+++ game.py
@@ -0,0 +1,24 @@
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Kyle Save The Day!')
+
+BG = pygame.image.load('game_background.jpg')
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+
+    pygame.quit
+
+if __name__ == "__main__":
+    main()
